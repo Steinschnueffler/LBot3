718b7f5582a4ce3e71a251aa96996852672050e0
Linus Dierheimer <Linus@Dierheimer.de>
2018-03-06 22:20:11 +1100

    Shutdown info if error

--- src/linus/discord/bot3/plugins/ShutdownPlugin.java
+++ src/linus/discord/bot3/plugins/ShutdownPlugin.java
@@ -1,6 +1,8 @@
 package linus.discord.bot3.plugins;
 
 import linus.discord.bot3.events.MessageReceivedEvt;
+import net.dv8tion.jda.core.events.ShutdownEvent;
+import net.dv8tion.jda.core.requests.CloseCode;
 
 public class ShutdownPlugin extends Plugin{
 
@@ -15,6 +17,13 @@ public class ShutdownPlugin extends Plugin{
 	protected void onMessageWithNameReceived(MessageReceivedEvt evt) {
 		PluginUtils.print("Shutting down... Bye!", evt.channel);
 		evt.bot.stop();
+	}
+	
+	@Override
+	public boolean onShutdown(ShutdownEvent event) {
+		if(event.getCloseCode() != CloseCode.GRACEFUL_CLOSE)
+			System.err.println("Application error: " + event.getCloseCode());
+		return false;
 	}
 
 }
